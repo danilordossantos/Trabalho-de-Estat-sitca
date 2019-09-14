061de4fe56f3806b547bb3e754cc19c3e61d5cb6
Flávio da Sílva Santos <flavio.diminuto@outlook.com>
2019-09-14 19:37:43 -0300

    Commit inicial

---
+++ TrabalhoEstatisticaApi/src/test/java/com/danilo-flavio/Estatistica/NativeTrabalhoApiIT.java
@@ -0,0 +1,9 @@
+package com.danilo-flavio.Estatistica;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeTrabalhoApiIT extends TrabalhoApiTest {
+
+    // Execute the same tests but in native mode.
+}

7842da328386f11aa36965a3a81fbf43b347395d
Flávio da Sílva Santos <flavio.diminuto@outlook.com>
2019-09-14 22:25:35 -0300

    Definindo estrutura de dados Amostras

--- TrabalhoEstatisticaApi/src/main/java/com/daniloflavio/Estatistica/model/Amostra.java
+++ TrabalhoEstatisticaApi/src/main/java/com/daniloflavio/Estatistica/model/Amostra.java
@@ -1,4 +1,36 @@
 package com.daniloflavio.Estatistica.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Amostra {
+    private HashMap<String, List<Integer>> registros;
+
+    public Amostra() {
+        registros = registrosZerados();
+    }
+
+    private void simulaRegistros(){
+        registros = registrosZerados();
+        insereDado("Bebes",3 );
+        insereDado("Crianças", 10);
+        insereDado("Jovems",20);
+        insereDado("Adultos", 10);
+        insereDado("Idosos",17);
+    }
+
+    private void insereDado(String nomeVariavel, int quantidade){
+        List<Integer> listaNumeros = new ArrayList<>();
+        listaNumeros.add(quantidade);
+        registros.put(nomeVariavel, listaNumeros);
+    }
+
+    private void removeRegistro(String chave){
+        this.registros.remove(chave);
+    }
+
+    private HashMap<String, List<Integer>> registrosZerados(){
+        return new HashMap<String, List<Integer>>();
+    }
 }
